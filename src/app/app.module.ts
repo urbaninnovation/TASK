c2b5bcfc69acd50c2aad5b26484f00a981833ebd
urbaninnovation <nils.schrum@gmx.de>
2019-08-19 16:25:38 +0200

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PageListComponent } from './page-list/page-list.component';
+import { TemplateTodoComponent } from './_template/template-todo/template-todo.component';
+import { TemplateTodoFormComponent } from './_template/template-todo-form/template-todo-form.component';
+import { TemplateHeaderComponent } from './_template/template-header/template-header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PageListComponent,
+    TemplateTodoComponent,
+    TemplateTodoFormComponent,
+    TemplateHeaderComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
